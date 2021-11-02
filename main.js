6eb63d7d280871f3bbd2aa07c65d85c1d3119421
Damien SALEM <d.salem@ecole-ipssi.net>
2021-11-02 14:20:40 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,8 @@
+document.addEventListener('mousemove', function(e) {
+    let circle = document.getElementById('circle');
+    const onMouseMove = (e) => {
+        circle.style.left = e.pageX + 'px';
+        circle.style.top = e.pageY + 'px';
+    }
+    document.addEventListener('mousemove', onMouseMove);
+});
